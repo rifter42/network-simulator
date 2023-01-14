b48e0d662096be0e1b01a570a8522ef6288f7874
racerf01 <racerf01@mail.ru>
2023-01-14 18:56:14 +0100

    Device Layer Prototype
    Device Layer Prototype

---
+++ Device/Device/src/cz/praguecityuniversity/Device.java
@@ -0,0 +1,14 @@
+package cz.praguecityuniversity;
+
+import java.sql.Connection;
+
+public class Device {
+
+    Computer Computer;
+    Router Router;
+    Switch Switch;
+
+    public Device getDevice(String deviceName, String macAddress, Connection Connection) {
+        return null;
+    }
+}
