a56a7dd6e9b0f25d3dd5f279be79cdb91ce7a53f
rifter42 <25634711+rifter42@users.noreply.github.com>
2023-01-15 21:36:06 +0100

    basic ping event adding back

--- src/SimulatedEnvironment.java
+++ src/SimulatedEnvironment.java
@@ -11,6 +11,17 @@ public class SimulatedEnvironment {
             List<Router> routers = config.get("routers");
             List<CabledConnection> connections = config.get("connections");
 
+            String IP1 = String.valueOf(computers.get(0).getIp());
+            String IP2 = String.valueOf(computers.get(1).getIp());
+
+            System.out.println("PING " + IP2 + "(" + IP2 + ") " + " 56(84) bytes of data");
+
+            if(EventHandler.ping(IP1, IP2)){
+                int n = 5;
+                for (int i = 1; i <= n; ++i) {
+                    System.out.println("64 bytes from " + IP2 + " : icmp_seq=" + i + " ttl=57 time=5.29 ms");
+                }
+            }
 
 
         } catch (IOException e) {
