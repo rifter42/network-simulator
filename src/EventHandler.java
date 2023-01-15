9ab9f853d27bf952e8f64903ab5d1590a7280afb
Feynman43 <mohammed.znitar@praguecollege.cz>
2023-01-15 19:10:46 +0100

    Implementing basic ping event

--- src/EventHandler.java
+++ src/EventHandler.java
@@ -1,2 +1,8 @@
+import java.io.IOException;
+
 public class EventHandler {
+    public static Boolean ping(String AddressSource, String AddressDestination) throws IOException {
+        boolean pingResult = Ping.execute(AddressSource, AddressDestination);
+        return pingResult;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,17 @@ public class Main {
         try {
             Map<String, List> config = new ConfigurationFactory().loadConfig("config.toml");
             List<Computer> computers = config.get("computers");
-            for (Computer computer: computers) {
-                System.out.println(computer.getIp());
+
+            String IP1 = String.valueOf(computers.get(0).getIp());
+            String IP2 = String.valueOf(computers.get(1).getIp());
+
+            System.out.println("PING " + IP2 + "(" + IP2 + ") " + " 56(84) bytes of data");
+
+            if(EventHandler.ping(IP1, IP2)){
+                int n = 5;
+                for (int i = 1; i <= n; ++i) {
+                    System.out.println("64 bytes from " + IP2 + " : icmp_seq=1 ttl=57 time=5.29 ms");
+                }
             }
 
             List<Router> routers = config.get("routers");
