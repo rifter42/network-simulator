f99ef35e22f501afc53c63ffdebd97f5f41c1c85
Rishabh Malik <rishabhmalik759@hotmail.com>
2023-01-22 13:11:40 +0100

    Updated IPv4 class

--- src/IPv4.java
+++ src/IPv4.java
@@ -6,7 +6,6 @@ public class IPv4 implements IPAddress {
     private int mask;
     IPv4(String IPAddressStr) throws InvalidIPAddress {
         parseIPv4Address(IPAddressStr);
-        String[] IPAddressArr = this.IPAddressStr.split("\\.");
         this.IPAddressInteger = this.parseAsVal(this.IPAddressStr);
     }
 
@@ -21,44 +20,6 @@ public class IPv4 implements IPAddress {
         String maskStr = split[1];
         this.IPAddressStr = addressStr;
         this.mask = Integer.parseInt(maskStr);
-    }
-
-    private ArrayList<ArrayList<Integer>> parseIPOctets(String[] IPAddressArr) {
-        ArrayList<ArrayList<Integer>> IPOctets = new ArrayList<>();
-        for (String IPSliceStr : IPAddressArr) {
-            ArrayList<Integer> octet = new ArrayList<>();
-            String binary = Integer.toBinaryString(Integer.parseInt(IPSliceStr));
-            for (int j = 0; j < binary.length(); j++) {
-                octet.add(Integer.parseInt(String.valueOf(binary.charAt(j))));
-            }
-            while (octet.size() < 8) {
-                octet.add(0, 0);
-            }
-            while (octet.size() > 8) {
-                octet.remove(0);
-            }
-            IPOctets.add(octet);
-        }
-        return IPOctets;
-    }
-
-    private ArrayList<ArrayList<Integer>> parseNetworkOctets(ArrayList<ArrayList<Integer>> IPOctets, int mask) {
-        ArrayList<ArrayList<Integer>> networkOctets = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
-            ArrayList<Integer> octet = IPOctets.get(i);
-            ArrayList<Integer> networkOctet = new ArrayList<>();
-            for (int j = 0; j < 8; j++) {
-                int ipBit = octet.get(j);
-                if (mask == 0) {
-                    networkOctet.add(0);
-                } else {
-                    networkOctet.add(ipBit);
-                    mask -= 1;
-                }
-            }
-            networkOctets.add(networkOctet);
-        }
-        return networkOctets;
     }
 
     private long parseAsVal(String IPAddressStr) {
